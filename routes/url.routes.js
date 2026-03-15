bf6eee86c76fb671a2a76a49e4521425e09f2e04
shivam <shivam7667@gmail.com>
2026-03-15 22:05:01 +0530

    remove all files

--- routes/url.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { shorturl, redirecting } from "../controllers/url.controller.js";
-
-const router = Router();
-
-router.post("/shorturl", shorturl);
-router.get("/:shortId", redirecting);
-
-export default router;
